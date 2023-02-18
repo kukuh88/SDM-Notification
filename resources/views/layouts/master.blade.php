2939d84c4b83b03df680e34711f6cebd90eec7c2
kukuh88 <kukuh.kurniawantkj2@gmail.com>
2023-02-18 16:35:16 +0700

    first commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <title>@yield('title', config('app.name', 'SDM PT DAK'))</title>
+
+    <!-- Favicons -->
+    <link href="{{ asset('admin/assets/img/ptdak.png') }}" rel="icon">
+    <link href="{{ asset('admin/assets/img/ptdak.png') }}" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="{{ asset('admin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" rel="stylesheet"
+        type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" rel="stylesheet">
+    @yield('vendor_css')
+
+    <!-- Template Main CSS File -->
+    <link href="{{ asset('admin/assets/css/style.css') }}" rel="stylesheet">
+    @yield('style_css')
+</head>
+
+<body>
+    @include('layouts.include.header')
+
+    @include('layouts.include.sidebar')
+
+    <main id="main" class="main">
+        @yield('content')
+    </main><!-- End #main -->
+
+    @include('layouts.include.footer')
+
+    @yield('modal')
+
+    <!-- Vendor JS Files -->
+    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+    <script src="{{ asset('admin/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendor/tinymce/tinymce.js') }}"></script>
+    <script src="{{ asset('admin/assets/vendor/php-email-form/validate.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js" type="text/javascript" charset="utf8">
+    </script>
+    @yield('vendor_js')
+
+    <!-- Template Main JS File -->
+    <script src="{{ asset('admin/assets/js/main.js') }}"></script>
+    <script src="{{ asset('admin/assets/js/ptdak.js') }}"></script>
+    @yield('script_js')
+</body>
+
+</html>
